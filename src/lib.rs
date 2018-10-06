5731c2a4ebf7c6c12a7293599be17c1b149cd257
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-10-07 02:52:38 +0900

    Basic math functions

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@ extern crate num_complex;
 extern crate num_traits;
 extern crate rand;
 
+use num_complex::Complex;
 use num_traits::*;
 
 pub use num_complex::Complex32 as c32;
@@ -34,52 +35,114 @@ pub trait Scalar: NumAssign + FromPrimitive + NumCast + Copy {
         + NumOps<Self::Real, Self::Complex>
         + NumOps<Self::Complex, Self::Complex>;
 
-    fn real<T: ToPrimitive>(re: T) -> Self::Real {
-        <Self::Real as NumCast>::from(re).unwrap()
-    }
-
+    /// Create a new real number
+    fn real<T: ToPrimitive>(re: T) -> Self::Real;
+    /// Create a new complex number
     fn complex<T: ToPrimitive>(re: T, im: T) -> Self::Complex;
 
-    fn re(self) -> Self::Real;
-    fn im(self) -> Self::Real;
-    fn as_c(self) -> Self::Complex;
+    /// Real part
+    fn re(&self) -> Self::Real;
+    /// Imaginary part
+    fn im(&self) -> Self::Real;
+    /// As a complex number
+    fn as_c(&self) -> Self::Complex;
+    /// Complex conjugate
+    fn conj(&self) -> Self;
+
+    /// Square root
+    fn sqrt(&self) -> Self;
+    /// Absolute value
+    fn abs(&self) -> Self::Real;
+    /// Sqaure of absolute value
+    fn abs_sqr(&self) -> Self::Real;
+
+    /// Exponential
+    fn exp(&self) -> Self;
+    /// Natural Logarithm
+    fn ln(&self) -> Self;
 }
 
 impl Scalar for f32 {
     type Real = f32;
     type Complex = c32;
-    fn re(self) -> Self::Real {
-        self
+
+    fn re(&self) -> Self::Real {
+        *self
     }
-    fn im(self) -> Self::Real {
+    fn im(&self) -> Self::Real {
         0.0
     }
+    fn real<T: ToPrimitive>(re: T) -> Self::Real {
+        NumCast::from(re).unwrap()
+    }
     fn complex<T: ToPrimitive>(re: T, im: T) -> Self::Complex {
-        c32 {
-            re: re.to_f32().unwrap(),
-            im: im.to_f32().unwrap(),
+        Complex {
+            re: NumCast::from(re).unwrap(),
+            im: NumCast::from(im).unwrap(),
         }
     }
-    fn as_c(self) -> Self::Complex {
-        c32::new(self, 0.0)
+    fn as_c(&self) -> Self::Complex {
+        c32::new(*self, 0.0)
+    }
+    fn conj(&self) -> Self {
+        *self
+    }
+
+    fn sqrt(&self) -> Self {
+        Float::sqrt(*self)
+    }
+    fn abs(&self) -> Self::Real {
+        Float::abs(*self)
+    }
+    fn abs_sqr(&self) -> Self::Real {
+        self * self
+    }
+    fn exp(&self) -> Self {
+        Float::exp(*self)
+    }
+    fn ln(&self) -> Self {
+        Float::ln(*self)
     }
 }
+
 impl Scalar for c32 {
     type Real = f32;
     type Complex = c32;
-    fn re(self) -> Self::Real {
+
+    fn re(&self) -> Self::Real {
         self.re
     }
-    fn im(self) -> Self::Real {
+    fn im(&self) -> Self::Real {
         self.im
+    }
+    fn real<T: ToPrimitive>(re: T) -> Self::Real {
+        NumCast::from(re).unwrap()
     }
     fn complex<T: ToPrimitive>(re: T, im: T) -> Self::Complex {
         c32 {
-            re: re.to_f32().unwrap(),
-            im: im.to_f32().unwrap(),
+            re: NumCast::from(re).unwrap(),
+            im: NumCast::from(im).unwrap(),
         }
     }
-    fn as_c(self) -> Self::Complex {
-        self
+    fn as_c(&self) -> Self::Complex {
+        *self
+    }
+    fn conj(&self) -> Self {
+        Complex::conj(&self)
+    }
+    fn sqrt(&self) -> Self {
+        Complex::sqrt(self)
+    }
+    fn abs(&self) -> Self::Real {
+        Complex::norm(self)
+    }
+    fn abs_sqr(&self) -> Self::Real {
+        Complex::norm_sqr(self)
+    }
+    fn exp(&self) -> Self {
+        Complex::exp(self)
+    }
+    fn ln(&self) -> Self {
+        Complex::ln(self)
     }
 }
